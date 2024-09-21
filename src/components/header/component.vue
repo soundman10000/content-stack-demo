52ecf253f86ce49735ec59a0184ac4f886a51ed7
Malley <jmalley@goempyrean.com>
2024-09-21 12:51:27 -0500

    get rid of all the boilerplate

---
+++ src/components/header/component.vue
@@ -0,0 +1,4 @@
+<script></script>
+<template>
+  <h1>Welcome To Jason's Content Stack Demo</h1>
+</template>
---
+++ src/views/IndexView.vue
@@ -0,0 +1,5 @@
+<script></script>
+
+<template>
+  <main>Hello World</main>
+</template>
