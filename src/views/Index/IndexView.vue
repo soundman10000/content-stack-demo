e7b9db58ab53e781b90d27dbc40748b2a7f0137c
Malley <jmalley@goempyrean.com>
2024-09-23 11:21:55 -0500

    little work on the model loading

--- src/views/Index/IndexView.vue
+++ src/views/Index/IndexView.vue
@@ -6,7 +6,8 @@ import { getContent, toViewModel } from './model'
 const csClient = inject('csDeliveryClient')
 
 const state = reactive({
-  welcomeBanner: null
+  content: {},
+  loaded: false
 })
 
 const onSucess = (state) => (result) => toViewModel(state)(result)
@@ -15,9 +16,11 @@ const onError = (d) => console.log(d, 'Error')
 getContent(csClient).then(onSucess(state), onError)
 </script>
 <template>
-  <header class="hero">
+  <header class="hero" v-if="state.loaded">
     <div class="hero-content">
-      <h1>{{ state.welcomeBanner }}</h1>
+      <h1 data-cslp="index.blt05b254af46ef63c2.en-us.welcome_banner">
+        {{ state.content.welcome_banner.text }}
+      </h1>
     </div>
   </header>
   <div>
--- src/views/Index/model.js
+++ src/views/Index/model.js
@@ -1,4 +1,5 @@
-import { propOr } from 'ramda'
+import { propOr, compose, objOf, path } from 'ramda'
+import { Utils } from 'contentstack'
 
 export const getContent = csClient => 
   csClient
@@ -6,7 +7,23 @@ export const getContent = csClient =>
   .Entry('blt05b254af46ef63c2')
   .toJSON()
   .fetch()
+  .then(addTags('index'))
+  .then(toContentModel('welcome_banner'))
 
 export const toViewModel = state => content => {
-  state.welcomeBanner = propOr('This is a place holder!', 'welcome_banner')(content)
+  console.log(content)
+  state.content = content
+  state.loaded = true
 }
+
+const addTags = typeId => c => {
+  Utils.addEditableTags(c, typeId, true, 'en-us')
+  return c
+}
+
+const toContentObject = name => content => ({ 
+  text: propOr('This is a place holder!', name)(content),
+  tag: path(['$', name, 'data-cslp'])(content),
+})
+
+const toContentModel = name => compose(objOf(name), toContentObject(name)) 
