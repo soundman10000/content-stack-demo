510c84fd4c99be64dfac08b58f662d42bf13b0b7
Malley <jmalley@goempyrean.com>
2024-09-25 09:16:03 -0500

    add in footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import Footer from './components/footer/component.vue'
 </script>
 
 <template>
-  <RouterView />
+  <div class="container main-content">
+    <RouterView />
+  </div>
+  <footer class="footer">
+    <Footer />
+  </footer>
 </template>
 
 <style scoped>
-.header {
-  color: var(--empyrean-blue);
-  background-color: var(--empyrean-blue-light);
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 40px; /* Height of the footer */
+  background-color: #f5f5f5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.main-content {
+  flex: 1;
 }
 </style>
---
+++ src/components/footer/component.vue
@@ -0,0 +1,7 @@
+<script>
+import './styles.css'
+</script>
+
+<template>
+  <footer><span class="text-muted">Powered By Content Stack </span></footer>
+</template>
