468bb473490ea1a659dea581797c5063629f093a
Malley <jmalley@goempyrean.com>
2024-09-26 15:05:26 -0500

    add the other icons

--- src/views/Home/components/Header/component.vue
+++ src/views/Home/components/Header/component.vue
@@ -8,12 +8,30 @@ import User from '../User/component.vue'
     </div>
     <nav>
       <ul class="nav">
+        <li class="nav-item icon-container">
+          <font-awesome-icon icon="fa-home" />
+        </li>
+        <li class="nav-item icon-container">
+          <span class="notification-bell">
+            <font-awesome-icon icon="fa-solid fa-bell" />
+            <span class="badge">1</span>
+          </span>
+        </li>
+        <li class="nav-item icon-container">
+          <font-awesome-icon icon="fa-envelope" />
+        </li>
+        <li class="nav-item icon-container">
+          <font-awesome-icon icon="fa-calendar" />
+        </li>
         <li class="nav-item icon-container" @mouseenter="userHoverOn" @mouseleave="userHoverOut">
-          <font-awesome-icon icon="fa-solid fa-user" />
+          <font-awesome-icon icon="fa-user" />
           <User v-if="state.showUser" class="user-modal"></User>
         </li>
+        <li class="nav-item icon-container">
+          <font-awesome-icon icon="fa-cog" />
+        </li>
         <li class="nav-item icon-container" @click="logout">
-          <font-awesome-icon icon="fa-solid fa-sign-out" />
+          <font-awesome-icon icon="fa-sign-out" />
         </li>
       </ul>
     </nav>
