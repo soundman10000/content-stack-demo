92272e21f42da3c075460865124e3a81fdb763c4
Malley <jmalley@goempyrean.com>
2024-09-26 15:10:39 -0500

    can take this out

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import './assets/main.css'
 import 'bootstrap/dist/css/bootstrap.css'
-import bootstrap from 'bootstrap/dist/js/bootstrap.js'
 import {createContentStackClient} from 'contentstackClient'
 import { createPinia } from 'pinia'
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -14,37 +13,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import Contentstack from 'contentstack'
-import ContentstackLivePreview from '@contentstack/live-preview-utils';
-
-const csDeliveryClient = Contentstack.Stack({
-  api_key: 'blt16b8a435e6f90caa',
-  delivery_token: 'csdb6a16940f5a9fb667945ecd',
-  live_preview: {
-    enable: true,
-    host: "rest-preview.contentstack.com", 
-    preview_token: "cs08b2313d23612772dc185b06"
-  },
-  environment: 'development',
-});
-
-ContentstackLivePreview.init({
- stackDetails: {
-     apiKey: "blt16b8a435e6f90caa",
-     environment: "development",
-     branch: "main"
-},
- clientUrlParams: {
-     protocol: "https",
-     host: "app.contentstack.com",
-     port: 443,
- },
-})
 
 createApp(App)
-  .provide('csDeliveryClient', csDeliveryClient)
+  .provide('csDeliveryClient', Contentstack)
   .provide('cmsClient', createContentStackClient())
   .use(createPinia())
-  .use(bootstrap)
   .use(router)
   .component('FontAwesomeIcon', FontAwesomeIcon)
   .mount('#app')
